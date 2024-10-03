8a9ae5af21aaac7b850e71e642bee732403cad96
t-bence <toth.bence.mihaly@gmail.com>
2024-10-03 15:25:44 +0200

    Rewrite to polars started

--- holdings.py
+++ holdings.py
@@ -1,29 +1,27 @@
 from datetime import date
 
 def read_holdings(filename: str):
-    import pandas as pd
+    import polars as pl
     holdings = (
-        pd
+        pl
         .read_csv(filename)[
             ["Instrumentum", "Készletnap", "Darab", "Beker. ár",
             "Bek. árf. érték", "Bek. költség", "Piaci ár", "Deviza",
             "Deviza-árfolyam (HUF)"]]
-        .dropna()
+        .drop_nulls()
     )
 
     holdings.columns = ("Instrument", "Date bought", "Number", "Unit cost",
          "Total cost", "Fee", "Current price", "Currency",
          "Current currency rate")
-    
-    def text_to_date(input: str) -> date:
-        pieces = [int(x) for x in input.strip().split(".")]
-        return date(*pieces)
-    
-    holdings["Date bought"] = holdings["Date bought"].apply(text_to_date)
 
-    holdings["Unit cost"] = pd.to_numeric(holdings["Unit cost"].str.replace(" ", ""))
-    holdings["Total cost"] = pd.to_numeric(holdings["Total cost"].str.replace(" ", ""))
-    holdings["Fee"] = pd.to_numeric(holdings["Fee"].str.replace(" ", ""))
-    holdings["Current price"] = pd.to_numeric(holdings["Current price"].str.replace(" ", ""))
-    
+    holdings = (
+        holdings
+        .with_columns(pl.col("Unit cost").str.replace(" ", "").cast(pl.Float64))
+        .with_columns(pl.col("Total cost").str.replace(" ", "").cast(pl.Float64))
+        .with_columns(pl.col("Fee").str.replace(" ", "").cast(pl.Float64))
+        .with_columns(pl.col("Current price").str.replace(" ", "").cast(pl.Float64))
+        .with_columns(pl.col("Date bought").str.to_date(format="%Y.%m.%d"))
+    )
+
     return holdings
--- returns.py
+++ returns.py
@@ -1,19 +1,39 @@
-import pandas as pd
+from typing import Any
+import polars as pl
 from rates import ExchangeRate
 
-def compute_returns(holdings: pd.DataFrame, rates: ExchangeRate):
+def compute_returns(holdings: pl.DataFrame, rates: ExchangeRate):
     
-    def past_rate(df: pd.DataFrame) -> float:
-        if df["Currency"] == "HUF":
+    def past_rate(row: tuple[Any]) -> float:
+        return 350.0
+        """
+        if pl.col("Currency") == "HUF":
             return 1.0
-        elif df["Currency"] == "EUR":
-            return rates.get_rate(df["Date bought"])
+        elif pl.col("Currency") == "EUR":
+            return rates.get_rate(pl.col("Date bought"))
         else:
-            raise ValueError(f"Not supported currency: {df["Currency"]}")
+            raise ValueError(f"Not supported currency: {pl.col("Currency")}")
+        """
+        
+    print(holdings.columns)
+        
+    holdings = (
+        holdings
+        .with_columns(
+            pl
+            .when(pl.col("Currency") == "HUF").then(pl.lit(1.0))
+            .when(pl.col("Currency") == "EUR").then(pl.lit(350))
+            .otherwise(pl.lit(None))
+            .alias("Purchase currency rate")
+        )
+        .with_columns(
+            (pl.col("Unit cost") * (pl.col("Current currency rate") - pl.col("Purchase currency rate")))
+            .alias("EURHUF gain")
+        )
+        .with_columns(
+            ((pl.col("Current price") - pl.col("Unit cost")) * pl.col("Current currency rate"))
+            .alias("Underlying gain")
+        )
+    )
     
-    holdings["Purchase currency rate"] = holdings.apply(past_rate, axis=1)
-
-    holdings["EURHUF gain"] = holdings[["Unit cost"]].multiply(holdings["Current currency rate"] - holdings["Purchase currency rate"], axis="index")
-    holdings["Underlying gain"] = (holdings["Current price"] - holdings["Unit cost"]).multiply(holdings["Current currency rate"], axis="index")
-
     print(holdings)
