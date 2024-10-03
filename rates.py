3bcbc263cb6324afcde21fbc0dfd63657d512766
t-bence <toth.bence.mihaly@gmail.com>
2024-10-03 23:22:28 +0200

    Date parsing rewritten

--- rates.py
+++ rates.py
@@ -8,27 +8,29 @@ def read_rates(filename: str) -> pl.DataFrame:
     dates = doc.xpath("//tr/td[1]/text()")
     rates = doc.xpath("//tr/td[2]/text()")
 
-    months_hungarian = {
-        "január": 1,
-        "február": 2,
-        "március": 3,
-        "április": 4,
-        "május": 5,
-        "június": 6,
-        "július": 7,
-        "augusztus": 8,
-        "szeptember": 9,
-        "október": 10,
-        "november": 11,
-        "december": 12
-    }
+    months_hungarian = (
+        "január",
+        "február",
+        "március",
+        "április",
+        "május",
+        "június",
+        "július",
+        "augusztus",
+        "szeptember",
+        "október",
+        "november",
+        "december"
+    )
+    month_numbers = [f"{i}." for i in range(1, 13)]
+
+    mapping = dict(zip(months_hungarian, month_numbers))
 
     return (
         pl
         .DataFrame(zip(dates, rates), schema=("Date", "EUR rate"))
         .with_columns(pl.col("EUR rate").str.replace(",", ".").cast(pl.Float64))
-        .with_columns(pl.col("Date").str.replace_many([".", ","], "").str.split(" "))
-        .with_columns(pl.col("Date").list.get(1).replace(months_hungarian).alias("month"))
-        .with_columns(pl.date(pl.col("Date").list.get(0), "month", pl.col("Date").list.get(2)).alias("Date bought"))
+        .with_columns(pl.col("Date").str.replace_many(mapping).str.split(",").list.get(0))
+        .with_columns(pl.col("Date").str.to_date(format="%Y. %-m. %-d.").alias("Date bought"))
         .select("Date bought", "EUR rate")
     )
